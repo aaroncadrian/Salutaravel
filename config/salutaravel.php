7abffeb2ccf751187eac7401843009a47a911696
Aaron Adrian <aaroncadrian@gmail.com>
2018-01-18 17:17:14 -0600

    Added config file with arrays of titles and suffixes

---
+++ config/salutaravel.php
@@ -0,0 +1,45 @@
+<?php
+
+return [
+
+    /*
+    |--------------------------------------------------------------------------
+    | Titles
+    |--------------------------------------------------------------------------
+    |
+    | This array consists of the titles that are used for the application.
+    |
+    */
+
+    'titles' => [
+        'Mr.',
+        'Ms.',
+        'Mrs.',
+
+        /*
+         * Beware that if you are deriving genders from the title that a user submits,
+         * "Dr." should be removed from this array, for this title does not represent a specific gender.
+         */
+        'Dr.',
+    ],
+
+    /*
+    |--------------------------------------------------------------------------
+    | Suffixes
+    |--------------------------------------------------------------------------
+    |
+    | This array consists of the suffixes that are used for the application.
+    |
+    */
+
+    'suffixes' => [
+        'Sr.',
+        'Jr.',
+        'V',
+        'IV',
+        'III',
+        'II',
+        'I',
+    ],
+
+];
