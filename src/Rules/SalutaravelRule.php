dacbe17141ef985b421ceba585739eb1335b84a2
Aaron Adrian <aaroncadrian@gmail.com>
2018-01-18 19:54:51 -0600

    Added comments to the Salutaravel Rule class

--- src/Rules/SalutaravelRule.php
+++ src/Rules/SalutaravelRule.php
@@ -7,8 +7,21 @@ use Illuminate\Contracts\Validation\Rule;
 
 abstract class SalutaravelRule implements Rule
 {
+    /**
+     * Stores a collection containing the salutation options from the config file.
+     *
+     * @var \Illuminate\Support\Collection
+     */
     protected $options;
 
+    /**
+     * Return the name of the salutation.
+     * The plural of this name will be used build the config key
+     *
+     * @see SalutaravelRule::configKey()
+     *
+     * @return string
+     */
     abstract protected function salutation();
 
     /**
@@ -23,7 +36,9 @@ abstract class SalutaravelRule implements Rule
     }
 
     /**
-     * Determine if the validation rule passes.
+     * Determine if the value is one of the options from the collection.
+     *
+     * @see SalutaravelRule::$options
      *
      * @param  string $attribute
      * @param  mixed $value
@@ -44,6 +59,11 @@ abstract class SalutaravelRule implements Rule
         return str_replace_array('?', [$this->salutation(), $this->optionsList()], 'The ? you selected is invalid.  Please select from the following: ?');
     }
 
+    /**
+     * Get a comma separated list of all options wrapped in quotation marks.
+     *
+     * @return string
+     */
     public function optionsList()
     {
         return $this->options->map(function($option){
@@ -51,6 +71,14 @@ abstract class SalutaravelRule implements Rule
         })->implode(', ');
     }
 
+    /**
+     * Get the config key of the options array.
+     *
+     * Salutation name comes from plural of value returned from salutation method.
+     * @see SalutaravelRule::salutation()
+     *
+     * @return string
+     */
     protected function configKey()
     {
         return str_replace_first('?', str_plural($this->salutation()), 'salutaravel.?');
