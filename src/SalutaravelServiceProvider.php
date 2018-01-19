69f708431838d21cc26985933581697dde03e22a
Aaron Adrian <aaroncadrian@gmail.com>
2018-01-18 21:29:10 -0600

    Wrote logic for Validator extension

--- src/SalutaravelServiceProvider.php
+++ src/SalutaravelServiceProvider.php
@@ -3,10 +3,14 @@
 namespace AaronAdrian\Salutaravel;
 
 
+use AaronAdrian\Salutaravel\Exceptions\SalutaravelException;
+use AaronAdrian\Salutaravel\Rules\ValidTitle;
+use Illuminate\Support\Facades\Validator;
 use Illuminate\Support\ServiceProvider;
 
 class SalutaravelServiceProvider extends ServiceProvider
 {
+
     public function boot()
     {
 
@@ -16,6 +20,30 @@ class SalutaravelServiceProvider extends ServiceProvider
     {
         $this->mergeConfigFrom(__DIR__.'/../config/salutaravel.php', 'salutaravel');
 
+    }
+
+    protected function bootValidationExtensions()
+    {
+        $rules = collect(config('salutaravel.rules'));
+
+        Validator::extend('salutation', function ($attribute, $value, $parameters) use ($rules) {
+
+            $parameters = collect($parameters);
+
+            if($parameters->isEmpty())
+            {
+                throw new SalutaravelException(str_replace_first('?', $rules->keys()->map(function($item) { return '"' . $item . '"'; })->implode(', '),'Salutation validation rule must contain one of the following parameters: ?'));
+            }
+
+            $ruleParameter = $parameters->first();
+
+            if(!$rules->keys()->contains($ruleParameter))
+            {
+                throw new SalutaravelException(str_replace_first('?', $ruleParameter, 'The parameter "?" is not valid for the Salutation validation rule'));
+            }
+
+            return (new ($rules[$ruleParameter]))->passes($attribute, $value);
+        });
     }
 
 }
