a8dd0258a583027bede0c09604103b35169bb174
Aaron Adrian <aaroncadrian@gmail.com>
2018-02-23 14:44:44 -0600

    Implemented options method

--- src/Rules/ValidSuffix.php
+++ src/Rules/ValidSuffix.php
@@ -3,10 +3,22 @@
 namespace AaronAdrian\Salutaravel\Rules;
 
 
+use Illuminate\Support\Collection;
+
 class ValidSuffix extends SalutaravelRule
 {
     protected function salutation()
     {
         return 'suffix';
+    }
+
+    /**
+     * Return collection that will be searched for the given value.
+     *
+     * @return Collection
+     */
+    protected function options()
+    {
+        return collect(config('salutaravel.suffixes'));
     }
 }
